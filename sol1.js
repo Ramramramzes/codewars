3e78bf4ac122f8bdf3b08430ceb8de8f9a628c88
Marat <marat@MacBook-Pro-Marat.local>
2023-07-22 21:15:59 +0300

    s1

---
+++ sol1.js
@@ -0,0 +1,20 @@
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// If anything in the text isn't a letter, ignore it and don't return it.
+// "a" = 1, "b" = 2, etc.
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+    let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let newArr = []
+    for(let i = 0; i < text.length; i++ ){
+        for(let j = 0; j< arr.length; j++){
+            if(text[i] == arr[j] || text[i] == arr[j].toUpperCase()){
+            newArr.push(++j)
+            }
+        }
+    }
+    return newArr.join().toString();
+}
+
+alphabetPosition("The sunset sets at twelve o' clock.");
