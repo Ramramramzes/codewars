d9f89ec9fd46b8ecc1d957de60543817bda9551d
Marat <68328146+Ramramramzes@users.noreply.github.com>
2023-07-28 11:38:37 +0000

    s19_from_ipad

---
+++ sol19.js
@@ -0,0 +1,12 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(a){
+    let sum = 0;
+    for (num of a) {
+        num > 0 ? sum+= num : false;
+    }
+    return sum;
+}
+
