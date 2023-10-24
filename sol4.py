3b4571f467627d189a6528949528181cc4dcc8fc
Marat <68328146+Ramramramzes@users.noreply.github.com>
2023-10-24 10:06:28 +0300

    add: sol4.py

---
+++ sol4.py
@@ -0,0 +1,9 @@
+# Create a combat function that takes the player's current health and the amount of damage recieved,
+
+# and returns the player's new health. Health can't be less than 0.
+
+def combat(health, damage):
+    health -= damage
+    if health <= 0:
+        health = 0
+    return health
